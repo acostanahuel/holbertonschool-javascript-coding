8484718ddf8743ae8b5eb3953019ca72b66f053c
acostanahuel <acostanahuel17@gmail.com>
2024-01-22 18:19:59 -0300

    testing

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function readAndPrintFile(filePath) {
+    try {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        console.log(content);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error(`Error: File not found - ${filePath}`);
+        } else {
+            console.error(`An error occurred: ${error.message}`);
+        }
+    }
+}
