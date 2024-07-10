ce38e930684526c21ab1940590e80180bb0a8253
acostanahuel <acostanahuel17@gmail.com>
2024-07-10 16:00:46 +0000

    request fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
-const url = process.argv[2];
+const urlApi = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request(urlApi, function (error, response, body) {
   if (error) {
-    console.error(error);
-  }
+    console.log(error); 
+  } else {
+    const fs = require('fs');
 
-  fs.writeFile(filePath, body, (content, error) => {
-    if (error) {
-      console.error(error);
-    }
-  });
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
+  }
 });
